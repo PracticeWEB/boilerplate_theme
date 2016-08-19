786692521eb19e2b754bffe3b14e8ea41f39579e
jason-carney-practiceweb <jason.carney@practiceweb.co.uk>
2016-08-19 12:46:21 +0100

    Meta tag added: mobile screen widths now supported by default

--- templates/system/page.tpl.php
+++ templates/system/page.tpl.php
@@ -84,6 +84,7 @@
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php print $language->language ?>" lang="<?php print $language->language ?>" dir="<?php print $language->dir ?>">
 <head>
   <?php print $head; ?>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
   <title><?php print $head_title; ?></title>
   <?php print $styles; ?>
   <?php print $scripts; ?>
